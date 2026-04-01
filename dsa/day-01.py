7614e254b2c0de4b95e6e8029812623b842aa117
Aatish <minhasaatish@gmail.com>
2026-04-01 21:03:46 +0530

    added comments

--- dsa/day-01.py
+++ dsa/day-01.py
@@ -1,4 +1,16 @@
 def maxProfit(prices):
+    """
+    Finds the maximum profit from buying and selling a stock once.
+    
+    Given an array of stock prices, returns the maximum profit that can be achieved
+    by buying on one day and selling on a later day. If no profit can be made, returns 0.
+    
+    Args:
+        prices (List[int]): List of stock prices where prices[i] is the price on day i.
+    
+    Returns:
+        int: The maximum profit possible.
+    """
     min_price = float('inf')
     max_profit = 0
 
@@ -15,6 +27,15 @@ def maxProfit(prices):
     return max_profit
 
 def reverseString(s):
+    """
+    Reverses a string in-place.
+    
+    Modifies the input list of characters s such that it is reversed.
+    Does not return anything, modifies s in-place.
+    
+    Args:
+        s (List[str]): List of characters to be reversed.
+    """
     left = 0
     right = len(s) - 1
 
@@ -26,6 +47,18 @@ def reverseString(s):
         right -= 1
 
 def isValid(s):
+    """
+    Checks if a string of parentheses is valid.
+    
+    A string is valid if open brackets are closed by the same type of brackets
+    and in the correct order. Uses a stack to keep track of opening brackets.
+    
+    Args:
+        s (str): String containing only parentheses characters.
+    
+    Returns:
+        bool: True if the string is valid, False otherwise.
+    """
     stack = []
     mapping = {
         ')': '(',
@@ -44,6 +77,19 @@ def isValid(s):
     return len(stack) == 0
 
 def twoSum(nums, target):
+    """
+    Finds two numbers in an array that add up to a target sum.
+    
+    Given an array of integers and a target sum, returns the indices of the two numbers
+    such that they add up to the target. Assumes exactly one solution exists.
+    
+    Args:
+        nums (List[int]): List of integers.
+        target (int): Target sum.
+    
+    Returns:
+        List[int]: List containing the two indices [i, j] where nums[i] + nums[j] == target.
+    """
     hash_map = {}
     for i, num in enumerate(nums):
         complement = target - num
